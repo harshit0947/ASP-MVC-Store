7ba49d9421febfaa9e4c0d2c82ae18e6cfcf3106
Harshit Agrawal <harshit.agrawal25@gmail.com>
2017-11-02 16:37:38 +0530

    My first commit

---
+++ Store/Store/Controllers/Api/ProductsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Store.Models;
+using System.Web.Mvc;
+using System.Web;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace Store.Controllers.Api
+{
+    public class ProductsController : ApiController
+    {
+        private ProductContext _context;
+
+        public ProductsController()
+        {
+            _context=new ProductContext();
+        }
+
+        //GET /api/products
+        public IEnumerable<Product> GetProducts()
+        {
+                var start = Convert.ToInt32(HttpContext.Current.Request.QueryString["start"]);
+                var length = Convert.ToInt32(HttpContext.Current.Request.QueryString["length"]);
+                var searchValue = HttpContext.Current.Request.QueryString["search[value]"];
+                int colnum = Convert.ToInt32(HttpContext.Current.Request.QueryString["order[0][column]"]);
+                string sortDir = HttpContext.Current.Request.QueryString["order[0][dir]"];
+                string colName = HttpContext.Current.Request.QueryString["columns[" + colnum + "][data]"];
+                Func<Product, Object> orderByFunc = null;
+
+                if (searchValue != "")
+                {
+                    return _context.Products.Where(p => p.ProductName.Contains(searchValue));
+                }
+                else
+                    switch (colName)
+                    {
+                        case "id":
+                            orderByFunc = p => p.Id;
+                            break;
+
+                        case "productName":
+                            orderByFunc = p => p.ProductName;
+                            break;
+
+                        case "price":
+                            orderByFunc = p => p.Price;
+                            break;
+                    }
+
+                var result = (sortDir.Equals("asc") ? (_context.Products.ToList().OrderBy(orderByFunc).Skip(start).Take(length)) : (_context.Products.ToList().OrderByDescending(orderByFunc).Skip(start).Take(length)));
+                return result;
+            }
+
+        //GET /api/products/1
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id);
+
+            if (product==null)
+            {
+                NotFound();
+            }
+            return Ok(product);
+        }
+
+        //POST /api/prpoducts
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult CreateProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                BadRequest();
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri+"/"+product.Id), product);
+        }
+
+        //PUT /api/products/1
+        [System.Web.Http.HttpPut]
+        public void UpdateProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var productInDb = _context.Products.SingleOrDefault(p => p.Id == id);
+
+            if (productInDb==null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                productInDb.ProductName = product.ProductName;
+                productInDb.Description = product.Description;
+                productInDb.Price = product.Price;
+                _context.SaveChanges();
+
+            }
+
+
+        }
+
+        //DELETE api/products/1
+        [System.Web.Http.HttpDelete]
+        public void DeleteProduct(int id)
+        {
+            var productInDb = _context.Products.SingleOrDefault(p => p.Id == id);
+
+            if (productInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Products.Remove(productInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Store/Store/Controllers/ProductController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web.Mvc;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext _context;
+        static List<Product> _products = new List<Product>();
+
+        public ProductController()
+        {
+            _context = new ProductContext();
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var productInDb= _context.Products.ToList();
+            return View(productInDb);
+        }
+
+        public ActionResult New()
+        {
+            return View("Add");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add");
+            }
+            else
+            {
+                var idExist = _context.Products.SingleOrDefault(p => p.Id == product.Id);
+                if (idExist != null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    _context.Products.Add(product);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index", "Product");
+                }
+                  
+            }
+          
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id );
+            if (product==null)
+            {
+                return HttpNotFound();
+            }
+            else
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Product");
+        }
+
+        public ActionResult UpdateProduct(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id);
+            if (product==null)
+            {
+                return HttpNotFound();
+            }
+            else
+            return View("UpdateForm", product);
+        }
+
+        [HttpPost]
+        public ActionResult SaveUpdate(Product product)
+        {
+            var productInDb = _context.Products.Single(p => p.Id == product.Id);
+
+            productInDb.Description = product.Description;
+            productInDb.Price = product.Price;
+            productInDb.ProductName = product.ProductName;
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Product");
+        }
+
+        public ActionResult Description(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id);
+            return View("Description", product);
+        }
+
+        public ActionResult Customer()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult AddCart(int id)
+        {
+            var listproduct = _products.FirstOrDefault(p => p.Id == id);
+
+            if (listproduct != null)
+            {
+                if (listproduct.Id == id)
+                {
+                    listproduct.QuantityInCart += 1;
+                }
+
+            }
+            else
+            {
+                Product product = _context.Products.SingleOrDefault(p => p.Id == id);
+                product.QuantityInCart = 1;
+                _products.Add(product);
+            }
+            TempData["products"] = _products;
+            return RedirectToAction("Customer","Product");
+        }
+
+        public ActionResult RemoveCart(int id)
+        {
+            List<Product> cartList = TempData.Peek("products") as List<Product>;
+            Product prod = cartList.Where(e => e.Id == id).FirstOrDefault();
+
+            if (prod.QuantityInCart>1)
+            {
+                prod.QuantityInCart -= 1;
+            }
+            else
+            {
+                cartList.Remove(prod);
+            }
+            _products = cartList;
+            TempData["products"] = _products;
+            return View("Cart");
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
